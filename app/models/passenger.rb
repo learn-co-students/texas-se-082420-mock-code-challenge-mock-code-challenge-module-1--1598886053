bd6093517e37126f0dc6b44f69a3dccfddc1d6f4
wellyyy <wesleydfaircloth@gmail.com>
2020-08-31 11:53:50 -0500

    submission for deadline

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,1 +1,34 @@
+class Passenger
+    attr_reader :name
 
+    @@all = []
+
+    def initilize(name)
+        @name = name
+    end
+
+    def name
+        @name
+    end
+
+    def self.all
+        @all
+    end
+
+    def rides
+
+    end
+
+    def drivers
+
+    end
+
+    def total_distance
+
+    end
+
+    def premium_members
+
+    end
+
+end

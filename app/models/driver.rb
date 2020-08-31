7319ec01e7491d7da7e2e7d2b44966fed25fbf3b
mlarcher531 <mlarcher531@gmail.com>
2020-08-31 11:27:31 -0500

    mock cc1

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,1 +1,29 @@
+class Driver
 
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        self.class.all << self
+    end
+
+    def passenger_names
+        Ride.all.collect{|r| r.passenger}
+    end
+
+    def rides
+        arr = Ride.all.select {|r| r.driver == self}
+    end
+
+    def self.all
+        @@all
+    end
+
+    def mileage_cap(miles= 0.0)
+        arr = Ride.all.select {|r| r.driver == self}
+        (arr.select {|r| r.distance > miles}).select {|r| r.driver}
+    end
+
+end

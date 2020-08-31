cdba109f008f5096d4a02f7f8924c15a2baa3729
wyounis25 <github email address>
2020-08-31 11:53:42 -0500

    submission for deadline

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,1 +1,53 @@
 
+class Passenger 
+
+    attr_accessor :total_distance
+    attr_reader :name 
+
+
+    @@all = []
+    @@premium_members = []
+
+
+
+    def initialize(name)
+    @name = name 
+    @rides = []
+    @drivers = []
+    @total_distance = 0
+    self.class.all << self
+    end 
+
+    def rides(ride)
+        @rides << ride
+        
+    end 
+
+    def drivers(drive)
+        @drivers << drive
+    end 
+
+     def total_distance(distance)
+        self.total_distance = distance + self.total_distance
+     end 
+
+
+    def self.all 
+        @@all
+    end 
+
+    def self.premium_members
+        Passenger.all do |distance|
+            if Passenger.total_distance > 100
+         @@premium_members << Passenger.name
+            end 
+        end 
+    end 
+
+
+
+
+
+
+
+end 
--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,1 +1,33 @@
 
+class Ride
+
+
+    attr_accessor :driver, :passenger, :distance
+    # @@all = []
+    def initialize (driver,passenger,distance)
+    @driver = driver
+    @passenger = passenger
+    @distance = distance
+    self.class.all << self
+    end 
+    
+
+    # def self.all
+    #     @@all
+    # end 
+
+    
+     def self.avrage_distance
+        Ride.all do |distance|
+            Ride.distance == self
+        
+        end 
+
+    end 
+
+
+ 
+
+
+
+end 

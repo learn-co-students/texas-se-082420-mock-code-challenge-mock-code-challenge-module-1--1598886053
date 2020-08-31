9458ce425ce35d6ccd3ac90d6e51c346dc8f2522
IainLR <iainrobertzon@gmail.com>
2020-08-31 11:15:32 -0500

    submission for deadline

--- tools/console.rb
+++ tools/console.rb
@@ -6,5 +6,16 @@ end
 
 
 # Put your variables here~!
+p1 = Passenger.new("Doug")
+p2 = Passenger.new("The proclaimers")
+
+d1 = Driver.new("Cyrano do Bergerac")
+d2 = Driver.new("Roxane")
+
+r1 = Ride.new(d1, p1, 4.6)
+r2 = Ride.new(d1, p1, 8.3)
+r3 = Ride.new(d1, p2, 500.3)
+r4 = Ride.new(d1, p2, 500.1)
+r5 = Ride.new(d2, p1, 300.3)
 
 binding.pry

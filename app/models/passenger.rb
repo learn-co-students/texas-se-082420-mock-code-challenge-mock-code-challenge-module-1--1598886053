9d15754ca69b41f79be69d0fbdce35647ca87c78
Far.k <farzana.mkarim@gmail.com>
2020-08-31 12:20:03 -0500

    done

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,1 +1,43 @@
+class Passenger
 
+    attr_writer :name
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def rides
+        Ride.each.select do |rides|
+            passenger.ride == passenger_ride
+        end
+    end
+
+    def driver
+    end
+
+
+    def total_distance
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.premium_members
+    end
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,1 +1,18 @@
+class Ride
 
+    attr_accessor :passenger, :driver, :distance
+
+    def initialize(passenger, driver, distance)
+        @passenger = passenger
+        @driver = driver
+        @distance = distance
+    end
+
+    def self.average_distance
+        Ride.all.collect do |distance|
+            distance.rides
+        end
+
+    end
+
+end

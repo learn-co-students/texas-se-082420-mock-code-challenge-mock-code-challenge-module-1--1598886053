23eea47dd366f0842dbe89d868dc074d07b02448
Alex Mata <68702441+denimcouch@users.noreply.github.com>
2020-08-31 11:57:41 -0500

    Revert "submission for deadline"

--- tools/console.rb
+++ tools/console.rb
@@ -6,17 +6,5 @@ end
 
 
 # Put your variables here~!
-alex = Passenger.new("Alex Mata")
-karo = Passenger.new("Karo Cantu")
-
-zack = Driver.new("Zack T.")
-brian = Driver.new("Brian G.")
-
-ride1 = Ride.new(brian, alex, 6)
-ride2 = Ride.new(brian, karo, 3.2)
-ride3 = Ride.new(zack, alex, 13.1)
-ride4 = Ride.new(brian, karo, 150)
-
 
 binding.pry
-puts "Thanks for riding with us!"
